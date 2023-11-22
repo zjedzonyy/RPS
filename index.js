8fc76528d5472fe1c28f7f4a4dffbb3762a2b757
artutom <138820458+zjedzonyy@users.noreply.github.com>
2023-11-22 19:41:34 +0000

    Dodano logikę gry

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
-var weapon = ["ROCK", "PAPER", "SCISSORS"]
+var weapons = ["ROCK", "PAPER", "SCISSORS"];
 
-function getComputerChoice(){
-    return weapon[Math.floor(Math.random() * weapon.length)];
+function getComputerChoice() {
+  return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
+function playSingleRound() {
+  const computerSelection = getComputerChoice();
+  const playerSelection = prompt("Choose your weapon: ").toUpperCase();
 
-function playSingleRound(){
-    const computerSelection = getComputerChoice();
-    const playerSelection = prompt("Dobierz broń: ").toUpperCase();
-
-    console.log(playerSelection);
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie!");
+    return "tie";
+  } else if (
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "PAPER" && computerSelection === "ROCK") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")
+  ) {
+    console.log("Player wins!");
+    return "player";
+  } else {
+    console.log("Computer wins!");
+    return "computer";
+  }
 }
+
+function game() {
+  let playerScore = 0,
+    computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const winner = playSingleRound();
+    if (winner === "player") {
+      playerScore++;
+    } else if (winner === "computer") {
+      computerScore++;
+    }
+  }
+
+  console.log(`Score: Player - ${playerScore}, Computer - ${computerScore}`);
+
+  if (playerScore > computerScore) {
+    console.log("Player wins the game!");
+  } else if (playerScore < computerScore) {
+    console.log("Computer wins the game!");
+  } else {
+    console.log("It's a tie in the game!");
+  }
+}
+
+game();
